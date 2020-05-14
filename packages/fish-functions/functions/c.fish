7b949b25790ed2ef393c0aed84b383f40a3f15a6
Will Stone <654103+will-stone@users.noreply.github.com>
2020-05-14 17:16:01 +0100

    Revert "Add new package: fish-functions"
    This reverts commit 26ea15091b8e88729f56335cec939b88aeba2ac4.

--- packages/fish-functions/functions/c.fish
+++
@@ -1,7 +0,0 @@
-function c --wraps code --description 'Visual Studio Code'
-  if count $argv > /dev/null
-    command code $argv
-  else
-    command code .
-  end
-end
--- packages/fish-functions/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --wraps ls --description 'List contents of directory'
-  command ls -AFG $argv
-end
